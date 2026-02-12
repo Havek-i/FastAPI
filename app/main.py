557653713040c4a2bae8aab6e70204c471ac452d
Havek-i <ivavasilev@gmail.com>
2026-02-12 12:56:53 +0700

    Вторая практическая работа (Приложение FastAPI POST)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from typing import Annotated
 import uvicorn
 
+# Константы
 PATH = 'app/templates'
 
+
+# Движок
 app = FastAPI()
 
+
+# Типы данных
+class SNum(BaseModel):
+    num1: int
+    num2: int
+
+
+# Маршруты
 @app.get("/")
 async def root():
     return FileResponse(f'{PATH}/index.html')
 
+@app.get("/calculate")
+async def calculate():
+    return FileResponse(f"{PATH}/calculate.html")
+
+@app.post("/calculate")
+async def calculate_post(data: Annotated[SNum, Form()]):
+    return {"result": data.num1 + data.num2}
+
+
+# Запуск файла
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
